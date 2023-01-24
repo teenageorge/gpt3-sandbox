2f2a17d2da75bd1335a3cdbd5de671c249bdc27d
teenamgeorge <teena.m.george@gmail.com>
2023-01-25 01:44:02 +0800

    updated model

--- gpt3-sql-demo.py
+++ gpt3-sql-demo.py
@@ -7,12 +7,12 @@ from api import demo_web_app
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 # Construct GPT object and show some examples
-gpt = GPT(engine="davinci", temperature=0.5, max_tokens=100)
+gpt = GPT(engine="text-davinci-002", temperature=0.7, max_tokens=100)
 
-gpt.add_example(Example("### select all instruments", " SELECT * FROM instrument"))
-gpt.add_example(Example("### select all instruments of type Bond",
+gpt.add_example(Example(" select all instruments", " SELECT * FROM instrument"))
+gpt.add_example(Example(" select all instruments of type Bond",
                         " SELECT * FROM instrument WHERE instrument_type_id = 'Bond'"))
-gpt.add_example(Example("### select all instruments of type Bond and maturity date 2021-12-31",
+gpt.add_example(Example(" select all instruments of type Bond and maturity date 2021-12-31",
                         " SELECT * FROM instrument WHERE instrument_type_id = 'Bond' AND maturity_date = "
                         "'2021-12-31'"))
 
@@ -20,7 +20,7 @@ gpt.add_example(Example("### select all instruments of type Bond and maturity da
 config = UIConfig(
     description="SQL from natural language",
     button_text="Generate SQL",
-    placeholder="Add a question",
+    placeholder="",
     show_example_form=False,
 )
 
